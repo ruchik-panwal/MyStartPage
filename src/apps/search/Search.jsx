c329655e9a2866597e919c22a823331370fbf593
ruchik-panwal <ruchik.panwal@gmail.com>
2026-01-17 21:18:05 +0000

    addedd state for keeping track of the search term

--- src/apps/search/Search.jsx
+++ src/apps/search/Search.jsx
@@ -1,5 +1,23 @@
-function Search(){
-    return <div className="bg-lavender-mist h-full w-full"></div>
+import { useState } from "react";
+import SearchBar from "./SearchBar";
+import SearchTerms from "./SearchTerms";
+
+function Search() {
+  const [myTerm, setMyTerm] = useState(""); //"Term" is what the user types in the input
+
+  // Setter Function for Input
+  function setTerm(val) {
+    setMyTerm(val);
+  }
+
+  return (
+    <div className="bg-lavender-mist h-full w-full p-5 gap-3 flex flex-col">
+      {/* Taking Input from the user*/}
+      <SearchBar termSetter={(val) => setTerm(val)} />
+      {/* Giving the input for filtering*/}
+      <SearchTerms getTerm={myTerm} />
+    </div>
+  );
 }
 
 export default Search;
