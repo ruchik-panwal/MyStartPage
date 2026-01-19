14f6992894dc9622024c601f37bc0992b326899f
ruchik-panwal <ruchik.panwal@gmail.com>
2026-01-19 06:56:43 +0000

    added hover and select ability to the buttons

--- src/apps/search/SearchTerms.jsx
+++ src/apps/search/SearchTerms.jsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
 import { terms } from "./terms";
 
-function SearchTerms({getTerm}) {
+function SearchTerms({ getTerm }) {
+  const [select, setSelect] = useState(0);
+
+  function setSelectFunc(val) {
+    setSelect(val);
+  }
+
   return (
-    <div className="w-full h-full border overflow-scroll overflow-x-hidden">
-      {terms.map(
-        (term, ind) => term.name.toLowerCase().includes(getTerm.toLowerCase()) && <div key={ind}>{term.name}</div>,
-      )}
+    <div className="flex flex-col w-full h-full border overflow-y-scroll overflow-x-hidden select-none">
+      {terms
+        .filter((term) =>
+          term.name.toLowerCase().includes(getTerm.toLowerCase()),
+        )
+        .map((term, ind) => (
+          <TermButton
+            key={ind}
+            term={term}
+            isSelected={ind == select}
+            onSelect={() => setSelectFunc(ind)}
+          />
+        ))}
     </div>
+  );
+}
+
+function TermButton({ term, ind, onSelect, isSelected }) {
+  return (
+    <button
+      onMouseEnter={onSelect}
+      className={`flex justify-between items-center ${isSelected ? "bg-black" : "bg-white"}`}
+    >
+      <div>
+        <img src="" alt="" /> <div>{term.name}</div>
+      </div>
+      <div className="">{term.count}</div>
+    </button>
   );
 }
 
