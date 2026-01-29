574060c2efa9bf6743b054718d4880e302ae04f2
ruchik-panwal <ruchik.panwal@gmail.com>
2026-01-29 16:23:05 +0000

    made a todo list comntainer and added logic

--- src/apps/todoList/TodoList.jsx
+++ src/apps/todoList/TodoList.jsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+import { todos as initialTodos } from "./List"; // Rename for clarity
+import Todo from "./Todo";
+import AddBtn from "./AddBtn";
+
 function Todolist() {
-    return <div className="bg-containerBg h-full w-full rounded-[10px]"></div>
+  const [todos, setTodos] = useState(initialTodos);
+
+  function inputChange(val, index) {
+    const newTodos = [...todos];
+    newTodos[index] = { ...newTodos[index], work: val };
+    setTodos(newTodos);
+  }
+
+  function statusChange(index) {
+    const newTodos = [...todos];
+    newTodos[index] = {
+      ...newTodos[index],
+      status: !newTodos[index].status,
+    };
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="bg-mainWhite h-full w-full rounded-[10px] overflow-hidden">
+      {/* TodoList Container */}
+      <div className="bg-containerBg h-full w-full">
+        {todos.map((todo, ind) => {
+          return (
+            <Todo
+              key={ind}
+              currentTodo={todo}
+              inputChange={(val) => inputChange(val, ind)}
+              setStatus={() => statusChange(ind)}
+            />
+          );
+        })}
+      </div>
+
+      {/* Button Container */}
+      <div></div>
+    </div>
+  );
 }
 
 export default Todolist;
