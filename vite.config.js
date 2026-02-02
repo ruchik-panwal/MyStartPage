bb747a4656518543a1d7bf99eb23990744eefd73
ruchik-panwal <ruchik.panwal@gmail.com>
2026-02-02 21:43:13 +0000

    added custom port

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), 
-    tailwindcss(),
-  ],
-
-})
+  server: {
+    port: 2603, // Change this to whatever port you want
+  },
+  plugins: [react(), tailwindcss()],
+});
